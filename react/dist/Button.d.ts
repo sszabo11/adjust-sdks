ce548decc89e6a7a79e15d384aec66575322cdb3
Rabbit muncher <sszabo1082@gmail.com>
2025-03-04 18:44:34 +1100

    bark

--- react/dist/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-interface ButtonProps {
-    onClick?: () => void;
-    children: React.ReactNode;
-}
-export default function Button({ onClick, children }: ButtonProps): React.JSX.Element;
-export {};
--- react/dist/Input.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-interface InputProps {
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
-declare const Input: React.FC<InputProps>;
-export default Input;
