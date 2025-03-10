b6a15da531d3b8cc80cb5f5b1816fc324e581746
Rabbit muncher <sszabo1082@gmail.com>
2025-03-10 19:23:21 +1100

    fix(core): change

--- core/lib/animations.ts
+++ core/lib/animations.ts
@@ -8,7 +8,7 @@ export type Animation = {
 
 export const animations = {
   fade: (options: Animation) => {
-    console.log("dde", options);
+    console.log("ddec", options);
     return `animate__fade${mappings[options.transition]}${mappings[options.direction]}`;
   },
   slide: (options: Animation) => {
