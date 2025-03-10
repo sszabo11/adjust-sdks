8fc0a82a537cec1c230642d01a78bc91ba24e3b8
Rabbit muncher <sszabo1082@gmail.com>
2025-03-10 18:53:04 +1100

    chore(release): 1.0.1 [meow]

--- core/lib/animations.ts
+++ core/lib/animations.ts
@@ -8,7 +8,7 @@ export type Animation = {
 
 export const animations = {
   fade: (options: Animation) => {
-    console.log(options);
+    console.log("de", options);
     return `animate__fade${mappings[options.transition]}${mappings[options.direction]}`;
   },
   slide: (options: Animation) => {
