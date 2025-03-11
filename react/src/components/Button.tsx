32e43f58de24b746897c99ae45922f254c46056e
Rabbit muncher <sszabo1082@gmail.com>
2025-03-11 17:36:59 +1100

    feat(react): idk

--- react/src/components/Button.tsx
+++ react/src/components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 export default function Button({ onClick, children }: ButtonProps) {
   function Click() {
-    console.log("react");
+    console.log("reactt");
     handleClick();
     onClick && onClick();
   }
