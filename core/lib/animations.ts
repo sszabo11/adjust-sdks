f92fcc3b9e2763abb6e8a903111d62fb9ea818bc
Rabbit muncher <sszabo1082@gmail.com>
2025-03-11 19:29:04 +1100

    fix(core): cdsffc

--- core/lib/animations.ts
+++ core/lib/animations.ts
@@ -8,7 +8,7 @@ export type Animation = {
 
 export const animations = {
   fade: (options: Animation) => {
-    console.log("ddeccccdx", options);
+    console.log("ddecccccdx", options);
     return `animate__fade${mappings[options.transition]}${mappings[options.direction]}`;
   },
   slide: (options: Animation) => {
