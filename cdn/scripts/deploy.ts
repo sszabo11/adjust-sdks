1f9292453c494f513c03b98dcd6a796a001607b4
Rabbit muncher <sszabo1082@gmail.com>
2025-03-11 19:08:49 +1100

    fix(cdn): cdsff

--- cdn/scripts/deploy.ts
+++ cdn/scripts/deploy.ts
@@ -34,7 +34,7 @@ async function deploy() {
     const response = await client.send(command);
     if (response.$metadata.httpStatusCode === 200) {
       console.log("Deployed code!!!!!!!!");
-      console.log("Ver")
+      console.log("dVer")
     }
   } catch (caught) {
     if (
