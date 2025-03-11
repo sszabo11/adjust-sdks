68060ec1d6025077fff05303bd9a2407111cbb7b
Rabbit muncher <sszabo1082@gmail.com>
2025-03-11 18:54:39 +1100

    fix(cdn): dfwe

--- cdn/index.ts
+++ cdn/index.ts
@@ -8,6 +8,6 @@ export function main() {
 
   adUnits.forEach((unit) => {
     console.log("cunrtic", unit);
-    console.log("ad unitt id: ", unit.dataset.adUnitId);
+    console.log("ad unitt idd: ", unit.dataset.adUnitId);
   });
 }
