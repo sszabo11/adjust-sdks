03bcdfb02d953d9a7af41b4000c2f7122bed4b06
Rabbit muncher <sszabo1082@gmail.com>
2025-03-12 14:06:53 +1100

    fix(cdn): meow

--- cdn/index.ts
+++ cdn/index.ts
@@ -8,6 +8,6 @@ export function main() {
 
   adUnits.forEach((unit) => {
     console.log("unicr", unit);
-    console.log("add uncittt idd: ", unit.dataset.adUnitId);
+    console.log("acdd uncittt idd: ", unit.dataset.adUnitId);
   });
 }
