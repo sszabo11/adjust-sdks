7df801e5dbdd940f58b1556d8d092a16fbc3f82a
Rabbit muncher <sszabo1082@gmail.com>
2025-03-12 14:52:44 +1100

    fix(cdn): gerd

--- cdn/index.ts
+++ cdn/index.ts
@@ -8,6 +8,6 @@ export function main() {
 
   adUnits.forEach((unit) => {
     console.log("unicr", unit);
-    console.log("ccaccdd uncittt idd: ", unit.dataset.adUnitId);
+    console.log("cccaccdd uncittt idd: ", unit.dataset.adUnitId);
   });
 }
